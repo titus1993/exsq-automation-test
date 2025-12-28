efbcf07658e9e13b25ef6be137b6236ba2985cae
Marvin Pivaral <titus.pivaral@gmail.com>
2025-12-27 21:17:56 -0600

    Add grep and report configuration

--- cypress.config.js
+++ cypress.config.js
@@ -5,10 +5,31 @@ module.exports = defineConfig({
   viewportWidth: 1920,
   viewportHeight: 1080,
   chromeWebSecurity: false,
+  env: {
+    grepFilterSpecs: true,
+    grepOmitFiltered: true,
+    burn: 0,
+  },
+  reporter: 'cypress-mochawesome-reporter', 
+  reporterOptions: {
+    reportDir: 'cypress/reports',
+    reportPageTitle: 'EXSQ Automation Test Report',
+    overwrite: true,
+    saveJson: true,
+    autoOpen: true,
+    charts: true,    
+    embeddedScreenshots: true,
+    inlineAssets: true,
+    saveAllAttempts: true,
+  },
   e2e: {
     baseUrl: 'https://demoqa.com/',
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      const { plugin: cypressGrepPlugin } = require('@cypress/grep/plugin')
+      require('cypress-mochawesome-reporter/plugin')(on);
+      cypressGrepPlugin(config)
+      return config
     },
   },
 });
